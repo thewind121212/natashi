3c4904eabdc5b813a73fdfa3e7e02e367ccbee75
linhtran007 <linh.tran@silentium.io>
2026-02-03 16:53:51 +0700

    fix: git ignore

--- playground/dist/server.d.ts
+++
@@ -1,4 +0,0 @@
-import { Express } from 'express';
-export declare function createServer(): Express;
-export declare function startServer(app: Express): Promise<void>;
-//# sourceMappingURL=server.d.ts.map
